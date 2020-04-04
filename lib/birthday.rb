70215cdde18c2dd10d510da9f7c2ec7dca1550f2
Evan Smith <professionalemail@zoho.com>
2020-04-04 02:36:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,4 +14,5 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+  puts "You are too old for this."
 end
